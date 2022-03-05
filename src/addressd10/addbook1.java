d901622f7f5d27e482e69f4a1e30557f44a50aba
Naisha-Pari <sneha.chaurasiya10@gmail.com>
2022-03-06 00:50:54 +0530

    addresday9

---
+++ src/addressd10/addbook1.java
@@ -0,0 +1,18 @@
+package addressd10;
+
+public class addbook1 {
+	int addressbookno;
+	String firstname;
+	String lastname;
+	String address;
+	String state;
+	String city;
+	addbook1(int addressbookno,String firstname,String lastname,String address,String State,String city){
+		this.addressbookno=addressbookno;
+		this.firstname=firstname;
+		this.lastname=lastname;
+		this.address=address;
+		this.state=state;
+		this.city=city;
+
+}}
